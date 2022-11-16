121d4e18fbe427b04739b7dd0436fff49bd98518
shi-nasu <110979121+shi-nasu@users.noreply.github.com>
2022-11-16 23:34:49 +0000

    update

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,4 +13,9 @@ class Public::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
+
+  def unsubscribe
+    @customer = Customer.find(params[:id])
+  end
+
 end
