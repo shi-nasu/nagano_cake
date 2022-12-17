2eb279adbc897d48af772684d0a3577bb9ab6b52
shi-nasu <110979121+shi-nasu@users.noreply.github.com>
2022-12-17 08:52:27 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログアウトの際、adminのログインページに遷移する記述
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      new_admin_session_path
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,7 @@
 class OrderDetail < ApplicationRecord
 
+  enum making_status: { cannot_manufactured: 0, waiting_production: 1, production: 2, completion_production: 3}
+
   belongs_to :item
   belongs_to :order
   def subtotal
