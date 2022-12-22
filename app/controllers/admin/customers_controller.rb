f22202971977b2c31400fb5b0bbc7d6a4bb6938a
shi-nasu <110979121+shi-nasu@users.noreply.github.com>
2022-12-22 06:56:57 +0000

    update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,9 +8,25 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
-    
+  #成功した時
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = 'successfully'
+      redirect_to admin_customer_path(@customer.id)
+  #失敗した時
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana,
+                                      :postal_code, :address, :telephone_number, :is_deleted, :created_at, :updated_at)
   end
 end
