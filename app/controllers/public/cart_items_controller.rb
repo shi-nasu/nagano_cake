76bbba587a4b1dbe4746e9e1e6e45c7614689ccd
shi-nasu <110979121+shi-nasu@users.noreply.github.com>
2022-12-22 10:16:37 +0000

    update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -26,6 +26,15 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
+    #↓再度同じ商品の数量を増やす記述↓
+    @cart_items = current_customer.cart_items
+    @cart_items.each do |cart_item|
+      if cart_item.item_id == @cart_item.item_id
+        new_amount = cart_item.amount + @cart_item.amount
+        cart_item.update(amount: new_amount)
+        return redirect_to cart_items_path
+      end
+    end
     @cart_item.save
     redirect_to cart_items_path
   end
